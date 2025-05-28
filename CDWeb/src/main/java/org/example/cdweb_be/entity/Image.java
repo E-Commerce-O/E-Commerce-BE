b460bbb533cdd24f7a1e11ed25ae20d4be85d57f
Quang Hieu <126405081+quangHieu2109@users.noreply.github.com>
2025-05-28 15:38:19 +0700

    fix upload file

---
+++ CDWeb/src/main/java/org/example/cdweb_be/entity/Image.java
@@ -0,0 +1,19 @@
+package org.example.cdweb_be.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String imageName;
+    @Lob
+    byte[] imageData;
+}
