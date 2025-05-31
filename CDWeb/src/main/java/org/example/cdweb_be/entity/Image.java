4338fe4519232161fc418672c6ab75a5c9c5b1d6
Quang Hieu <126405081+quangHieu2109@users.noreply.github.com>
2025-05-31 12:11:33 +0700

    update upload image api

--- CDWeb/src/main/java/org/example/cdweb_be/entity/Image.java
+++ CDWeb/src/main/java/org/example/cdweb_be/entity/Image.java
@@ -5,15 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 public class Image {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+//    @GeneratedValue(strategy = GenerationType.UUID)
     String id;
     String imageName;
     @Lob
     byte[] imageData;
+    public Image() {
+        this.id = generateIdWithExtension();
+    }
+
+    private String generateIdWithExtension() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString() + ".jpg"; // Kết hợp UUID với chuỗi .jpg
+    }
 }
